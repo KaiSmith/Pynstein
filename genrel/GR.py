6c80ae27a73e1f5cc577574b504302f7bfd7e56b
Kai Smith <KaiSmith3027@gmail.com>
2014-06-03 14:26:28 -0400

    Implemented Reimann tensor

--- genrel/GR.py
+++ genrel/GR.py
@@ -19,12 +19,26 @@ def christoffel(metric, metric_key):
                 for delta in range(4):                    
                     total += sympy.Matrix(metric).inv()[alpha, delta] * (sympy.diff(metric[delta][beta], metric_key[gamma]) + 
                             sympy.diff(metric[delta][gamma], metric_key[beta]) - sympy.diff(metric[beta][gamma], metric_key[delta]))
-                symbols[alpha][beta][gamma] = sympy.cancel(1*total/2)
+                symbols[alpha][beta][gamma] = sympy.cancel(total/2)
     return symbols
                     
 
-def reimann_tensor(chris):
-    pass
+def reimann_tensor(chris_sym, metric_key):
+    #reimann tensor calculated with the first index as an upper index and all of the other ones as lower indecies
+    reimann = np.empty((4, 4, 4, 4), dtype = type(sympy.Symbol('Test')*1))
+    for alpha in range(4):
+        for beta in range(4):
+            for gamma in range(4):
+                for delta in range(4):
+                    total = 0
+                    total += sympy.diff(chris_sym[alpha][beta][delta], metric_key[gamma])
+                    total += sympy.diff(chris_sym[alpha][beta][gamma], metric_key[delta])
+                    for epsilon in range(4):
+                        total += chris_sym[alpha][gamma][epsilon]*chris_sym[epsilon][beta][delta]
+                        total -= chris_sym[alpha][delta][epsilon]*chris_sym[epsilon][beta][gamma]
+                    reimann[alpha][beta][gamma][delta] = sympy.cancel(total)
+    return reimann
+
 
 def ricci_tensor(reimann):
     pass
@@ -50,4 +64,6 @@ if __name__ == "__main__":
     metric = np.diag([-1, a**2/(1-k*r**2), a**2*r**2,a**2*r**2*sympy.sin(theta)**2])
     metric_key = [t, r, theta, phi]
     c = christoffel(metric, metric_key)
-    pprint(c)
+    print("Christoffel symbols calculated")
+    r = reimann_tensor(c, metric_key)
+    print("Reimann tensor calculated")
