9bf4afeea85c147d666412e38b890f5e5751ead3
Kai Smith <KaiSmith3027@gmail.com>
2014-06-03 14:02:12 -0400

    Christoffel symbols ooutputting, but have minor issues like extra negative signs

--- genrel/GR.py
+++ genrel/GR.py
@@ -11,15 +11,16 @@ import sympy
 def christoffel(metric, metric_key):
     #symols will be a rank 3 tensor. The first index will correspond to the upper
     #index and the next two will correspond to the lower indecies.
-    symbols = [[[],[],[],[]],[[],[],[],[]],[[],[],[],[]],[[],[],[],[]]]
+    symbols = np.empty((4, 4, 4), dtype = type(sympy.Symbol('Test')*1))
     for alpha in range(4):
         for beta in range(4):
             for gamma in range(4):
                 total = 0
-                for delta in range(4):
-                    total += 0.5*np.matrix(metric).I[alpha][delta] * (sympy.diff(metric[delta][beta], metric_key[gamma]) + 
+                for delta in range(4):                    
+                    total += sympy.Matrix(metric).inv()[alpha, delta] * (sympy.diff(metric[delta][beta], metric_key[gamma]) + 
                             sympy.diff(metric[delta][gamma], metric_key[beta]) + sympy.diff(metric[beta][gamma], metric_key[delta]))
-                symbols[alpha][beta][gamma] = total
+                symbols[alpha][beta][gamma] = sympy.cancel(1*total/2)
+    return symbols
                     
 
 def reimann_tensor(chris):
@@ -35,6 +36,8 @@ def einstein_tensor(ricci_t, ricci_s, metric):
     pass
 
 if __name__ == "__main__":
+    from pprint import pprint
+    
     t = sympy.Symbol('t')
     r = sympy.Symbol('r')
     theta = sympy.Symbol('theta')
@@ -44,8 +47,7 @@ if __name__ == "__main__":
 
 
 
-    metric = np.array([[-1, 0, 0, 0],[0, a**2/(1-k*r**2), 0, 0],[0, 0, a**2*r**2, 0],[0, 0, 0, a**2*r**2*sympy.sin(theta)**2]])
+    metric = np.diag([-1, a**2/(1-k*r**2), a**2*r**2,a**2*r**2*sympy.sin(theta)**2])
     metric_key = [t, r, theta, phi]
     c = christoffel(metric, metric_key)
-    from pprint import pprint
     pprint(c)
