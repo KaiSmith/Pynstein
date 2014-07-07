6b601b9c96a7886a5a24420b40a27cf975b3139f
David Clark <davidclark@berkeley.edu>
2014-07-07 16:26:47 -0400

    Did some calculations

--- genrel/BianchiAnisotropicSE.py
+++ genrel/BianchiAnisotropicSE.py
@@ -10,21 +10,23 @@ import sympy as sp
 import scipy.integrate
 import matplotlib.pyplot as pplot
 
-a0 = 0.5
+a0 = 100.0
 b0 = 1.0
-c0 = 1.5
+c0 = 1.0
 
 a_dot0 = 1.0
-b_dot0 = 1.5
-c_dot0 = 2.0
+b_dot0 = 10.0
+c_dot0 = 20.0
 
-p0 = 1
+rho0 = 10.0
+p0 = (rho0/3.0)*((a_dot0/a0 + b_dot0/b0 + c_dot0/c0)/(a_dot0 + b_dot0 + c_dot0))
 
+#TODO: add in factors of 8*pi*G
 p1 = a0**2*b0*c0*p0
 p2 = a0*b0**2*c0*p0
 p3 = a0*b0*c0**2*p0
 
-t = np.linspace(0, 10, 50)
+t = np.linspace(0, 100000, 1000)
 
 def dydt(y, t):
 	a, a_dot, b, b_dot, c, c_dot = y
@@ -32,7 +34,6 @@ def dydt(y, t):
 	b_dot_dot = ((b*a_dot*c_dot)/(a*c) - (b_dot*a_dot)/a - (b_dot*c_dot)/c + p3/(a*c**2) + p1/(a**2*c) - p2/(a*b*c))/2.0
 	c_dot_dot = ((c*a_dot*b_dot)/(a*b) - (c_dot*a_dot)/a - (c_dot*b_dot)/b + p1/(a**2*b) + p2/(a*b**2) - p3/(a*b*c))/2.0
 	return [a_dot, a_dot_dot, b_dot, b_dot_dot, c_dot, c_dot_dot]
-
 
 def plot_stuff():
 	y0 = [a0, a_dot0, b0, b_dot0, c0, c_dot0]
@@ -49,6 +50,43 @@ def plot_stuff():
 	V = [value[0]*value[2]*value[4] for value in y]
 	#V_dot = [value[1]*value[2]*value[4] + value[0]*value[3]*value[4] + value[0]*value[2]*value[5] for value in y]
 
+	"""
+	num = [a0/value[0] - b0/value[2] for value in y]
+	denom = [((a0*b0*c0)/(value[0]*value[2]*value[4]))**(1.0/3.0) - c0/value[4] for value in y]
+	frac = [num[i]/denom[i] for i in range(len(num))]
+	"""
+
+	g_prime = [p0*(a0/value[0] - b0/value[2]) for value in y]
+	f_prime = [rho0*((a0*b0*c0)/(value[0]*value[2]*value[4]))**(1.0/3.0) - p0*(c0/value[4]) for value in y]
+	ratio = [g_prime[i]/f_prime[i] for i in range(0, len(g_prime))]
+	limit = [(1 - ratio[i])/(1 + ratio[i]) for i in range(len(ratio))]
+	print(limit)
+
+	A_over_B = [(value[1]/value[0])/(value[3]/value[2]) for value in y]
+	print(A_over_B)
+
+	ratio_of_ratios = [limit[i]/A_over_B[i] for i in range(len(A_over_B))]
+	print(ratio_of_ratios)
+
+	"""
+	step_size = t[1] - t[0]
+	f = [0]
+	for i in range(1, len(f_inside_integral)):
+		f.append(f[i-1] + f_inside_integral[i]*step_size)
+	g = [0]
+	for i in range(1, len(g_inside_integral)):
+		g.append(g[i-1] + g_inside_integral[i]*step_size)
+
+	g_over_f = [g[i]/f[i] for i in range(1, len(f))]
+
+	pplot.scatter(t[1:], np.float64(g_over_f), c = 'r')
+	pplot.show()
+
+	A_over_B = [(value[1]/value[0])/(value[3]/value[2]) for value in y]
+	print(A_over_B)
+	"""
+
+	"""
 	pplot.scatter(t, np.float64(A), c = 'r')
 	pplot.scatter(t, np.float64(B), c = 'g')
 	pplot.scatter(t, np.float64(C), c = 'b')
@@ -58,5 +96,14 @@ def plot_stuff():
 	pplot.scatter(t, np.float64(b), c = 'g')
 	pplot.scatter(t, np.float64(c), c = 'b')
 	pplot.show()
+	"""
+
+	"""
+
+	print(frac)
+	pplot.scatter(t, np.float64(frac), c = 'r')
+	pplot.show()
+	"""
+
 
 plot_stuff()
