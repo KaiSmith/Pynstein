76f44a2ea84edc18c4a3de4f1e21373f92de77b6
David Clark <davidclark@berkeley.edu>
2014-07-09 17:02:28 -0400

    Changed values

--- genrel/BianchiAnisotropicSE.py
+++ genrel/BianchiAnisotropicSE.py
@@ -10,102 +10,109 @@ import sympy as sp
 import scipy.integrate
 import matplotlib.pyplot as pplot
 
-a0 = 100.0
+a0 = 1.0
 b0 = 1.0
 c0 = 1.0
 
 a_dot0 = 1.0
-b_dot0 = 10.0
-c_dot0 = 20.0
+b_dot0 = 1.0
+c_dot0 = 1.0
 
 A0 = a_dot0/a0
 B0 = b_dot0/b0
 C0 = c_dot0/c0
 
-I0 = A0*B0+B0*C0+A0*C0
 omega0 = 1
+
+t = np.linspace(0, 1000000000, 1000)
+
+I0 = A0*B0+B0*C0+A0*C0
 H0 = A0+B0+C0
+V0 = a0*b0*c0
 
-p0 = I0*omega0/3*H0/(a_dot0+b_dot0+c_dot0)
-
-p1 = a0**2*b0*c0*p0
-p2 = a0*b0**2*c0*p0
-p3 = a0*b0*c0**2*p0
-
-t = np.linspace(0, 100000, 1000)
+chi0 = (omega0*I0*V0*H0)/(3*(a_dot0+b_dot0+c_dot0))
 
 def dydt(y, t):
 	a, a_dot, b, b_dot, c, c_dot = y
-	a_dot_dot = ((a*b_dot*c_dot)/(b*c) - (a_dot*c_dot)/c - (a_dot*b_dot)/b + p3/(b*c**2)+ p2/(b**2*c) - p1/(a*b*c))/2.0
-	b_dot_dot = ((b*a_dot*c_dot)/(a*c) - (b_dot*a_dot)/a - (b_dot*c_dot)/c + p3/(a*c**2) + p1/(a**2*c) - p2/(a*b*c))/2.0
-	c_dot_dot = ((c*a_dot*b_dot)/(a*b) - (c_dot*a_dot)/a - (c_dot*b_dot)/b + p1/(a**2*b) + p2/(a*b**2) - p3/(a*b*c))/2.0
+	a_dot_dot = (a/2.0)*((chi0/(a*b*c))*(a0/a - b0/b - c0/c) - (a_dot*b_dot)/(a*b) - (a_dot*c_dot)/(a*c) + (b_dot*c_dot)/(b*c))
+	b_dot_dot = (b/2.0)*((chi0/(a*b*c))*(-a0/a + b0/b - c0/c) - (a_dot*b_dot)/(a*b) + (a_dot*c_dot)/(a*c) - (b_dot*c_dot)/(b*c))
+	c_dot_dot = (c/2.0)*((chi0/(a*b*c))*(-a0/a - b0/b + c0/c) + (a_dot*b_dot)/(a*b) - (a_dot*c_dot)/(a*c) - (b_dot*c_dot)/(b*c))
 	return [a_dot, a_dot_dot, b_dot, b_dot_dot, c_dot, c_dot_dot]
 
 def plot_stuff():
-    y0 = [a0, a_dot0, b0, b_dot0, c0, c_dot0]
-    y = scipy.integrate.odeint(dydt, y0, t)
+	y0 = [a0, a_dot0, b0, b_dot0, c0, c_dot0]
+	y = scipy.integrate.odeint(dydt, y0, t)
 
-    a = [value[0] for value in y]
-    b = [value[2] for value in y]
-    c = [value[4] for value in y]
+	a = [value[0] for value in y]
+	b = [value[2] for value in y]
+	c = [value[4] for value in y]
 
-    A = [value[1]/value[0] for value in y]
-    B = [value[3]/value[2] for value in y]
-    C = [value[5]/value[4] for value in y]
-    
-    V = [value[0]*value[2]*value[4] for value in y]
-    #V_dot = [value[1]*value[2]*value[4] + value[0]*value[3]*value[4] + value[0]*value[2]*value[5] for value in y]
+	A = [value[1]/value[0] for value in y]
+	B = [value[3]/value[2] for value in y]
+	C = [value[5]/value[4] for value in y]
+
+	V = [value[0]*value[2]*value[4] for value in y]
+	#V_dot = [value[1]*value[2]*value[4] + value[0]*value[3]*value[4] + value[0]*value[2]*value[5] for value in y]
 
 	"""
-	num = [a0/value[0] - b0/value[2] for value in y]
-	denom = [((a0*b0*c0)/(value[0]*value[2]*value[4]))**(1.0/3.0) - c0/value[4] for value in y]
-	frac = [num[i]/denom[i] for i in range(len(num))]
-	"""
-
 	g_prime = [p0*(a0/value[0] - b0/value[2]) for value in y]
 	f_prime = [rho0*((a0*b0*c0)/(value[0]*value[2]*value[4]))**(1.0/3.0) - p0*(c0/value[4]) for value in y]
 	ratio = [g_prime[i]/f_prime[i] for i in range(0, len(g_prime))]
 	limit = [(1 - ratio[i])/(1 + ratio[i]) for i in range(len(ratio))]
-	print(limit)
+	print(limit[-1])
+	"""
 
+	"""
 	A_over_B = [(value[1]/value[0])/(value[3]/value[2]) for value in y]
 	print(A_over_B)
-
-	ratio_of_ratios = [limit[i]/A_over_B[i] for i in range(len(A_over_B))]
-	print(ratio_of_ratios)
+	"""
 
 	"""
 	step_size = t[1] - t[0]
 	f = [0]
-	for i in range(1, len(f_inside_integral)):
-		f.append(f[i-1] + f_inside_integral[i]*step_size)
+	for i in range(1, len(f_prime)):
+		f.append(f[i-1] + f_prime[i]*step_size)
 	g = [0]
-	for i in range(1, len(g_inside_integral)):
-		g.append(g[i-1] + g_inside_integral[i]*step_size)
+	for i in range(1, len(g_prime)):
+		g.append(g[i-1] + g_prime[i]*step_size)
+	num = [f[i] + g[i] for i in range(len(f))]
+	denom = [f[i] - g[i] for i in range(len(f))]
+	ratio = [num[i]/denom[i] for i in range(1, len(f))]
 
+	pplot.scatter(t[1:], np.float64(ratio), c = 'r')
+	pplot.show()
+	"""
+
+	"""
 	g_over_f = [g[i]/f[i] for i in range(1, len(f))]
 
 	pplot.scatter(t[1:], np.float64(g_over_f), c = 'r')
 	pplot.show()
-
-	A_over_B = [(value[1]/value[0])/(value[3]/value[2]) for value in y]
-	print(A_over_B)
 	"""
 
 	"""
-	pplot.scatter(t, np.float64(A), c = 'r')
-	pplot.scatter(t, np.float64(B), c = 'g')
-	pplot.scatter(t, np.float64(C), c = 'b')
+	B_over_C = [A[i]/B[i] for i in range(len(t))]
+	C_over_A = [C[i]/A[i] for i in range(len(t))]
+	B_over_A = [B[i]/A[i] for i in range(len(t))]
+	
+	pplot.scatter(t, A, c = 'r')
+	pplot.scatter(t, B, c = 'g')
+	pplot.scatter(t, C, c = 'b')
 	pplot.show()
 
-	pplot.scatter(t, np.float64(a), c = 'r')
-	pplot.scatter(t, np.float64(b), c = 'g')
-	pplot.scatter(t, np.float64(c), c = 'b')
+	pplot.scatter(t, a, c = 'r')
+	pplot.scatter(t, b, c = 'g')
+	pplot.scatter(t, c, c = 'b')
 	pplot.show()
 	"""
 
-	"""
+	gamma0 = H0/(3.0*(a_dot0+b_dot0+c_dot0))
+	f_prime = [(V0/V[i])**(1.0/3.0) - gamma0*(c0/c[i]) for i in range(len(t))]
+	g_prime = [gamma0*(a0/a[i] - b0/b[i]) for i in range(len(t))]
+	ratio = [g_prime[i] - f_prime[i] for i in range(len(t))]
+	print(ratio[-1])
 
+	"""
 	print(frac)
 	pplot.scatter(t, np.float64(frac), c = 'r')
 	pplot.show()
